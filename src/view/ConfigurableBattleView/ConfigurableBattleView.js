ba553baa4d5568e5aa2f04329f2bf3c8c93eaf69
felixdae <felixdae@gmail.com>
2026-05-19 00:20:44 +0800

    fix: match PvE proportions 5/14/5 in ConfigurableBattleView

--- src/view/ConfigurableBattleView/ConfigurableBattleView.js
+++ src/view/ConfigurableBattleView/ConfigurableBattleView.js
@@ -321,7 +321,8 @@ function ConfigurableBattleView() {
 
             <div className={'doudizhu-view-container'}>
                 <Layout.Row style={{ height: '540px' }}>
-                    <Layout.Col style={{ height: '100%' }} span="17">
+                    <Layout.Col span="5" />
+                    <Layout.Col style={{ height: '100%' }} span="14">
                         <div style={{ height: '100%' }}>
                             <Paper className={'doudizhu-gameboard-paper'} elevation={3}>
                                 <DoudizhuGameBoard
@@ -339,7 +340,7 @@ function ConfigurableBattleView() {
                             </Paper>
                         </div>
                     </Layout.Col>
-                    <Layout.Col span="7" style={{ height: '100%' }}>
+                    <Layout.Col span="5" style={{ height: '100%' }}>
                         <Paper className={'doudizhu-probability-paper'} elevation={3} style={{ height: '100%', overflow: 'auto' }}>
                             <div style={{ padding: '16px' }}>
                                 <div style={{ fontWeight: 'bold', marginBottom: '12px' }}>
